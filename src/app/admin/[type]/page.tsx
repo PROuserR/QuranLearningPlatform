a007be11d671b47fcbd2c85405efe124fc1f69a9
M.Rami Alshaar <rshaar559@gmail.com>
2026-01-16 17:30:36 +0300

    working on mobile view for admin page

--- src/app/admin/[type]/page.tsx
+++ src/app/admin/[type]/page.tsx
@@ -2,9 +2,10 @@
 import DataTable from "@/components/datatable";
 
 
-function Page() {
+function Page({ params }: { params: { type: string } }) {
     return (
         <main className="w-full flex flex-col gap-y-8">
+            {params.type}
             <section className="text-4xl font-bold ml-auto">
                 جدول الطلاب
             </section>
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { PieChart, Pie, Cell } from 'recharts';
+import Link from 'next/link';
 
 const data = [
   { name: 'محرم', number: 7 },
@@ -37,67 +38,67 @@ function Page() {
           هذه احصائيات عن المنصة يمكنك الاطلاع على ما يحصل في المنصة
         </span>
       </section>
-      <section className='flex text-2xl text-green-50 gap-x-8 h-full'>
-        <div className='w-2/5 h-full grid grid-cols-2 gap-x-6 gap-y-12 text-end'>
+      <section className='flex flex-col md:flex-row text-sm md:text-xl text-green-50 gap-y-8 md:gap-x-8 h-full'>
+        <div className='w-full md:w-2/5 h-full grid grid-cols-2 gap-x-6 gap-y-12 text-end'>
           <div className='flex flex-col items-center justify-end w-full p-4 rounded-3xl bg-green-800 shadow-xl shadow-black gap-y-16'>
             <div className='flex flex-row-reverse items-center justify-center w-full gap-x-24'>
               <span className=''> عدد الطلاب  </span>
               <span className='font-bold'> 221 </span>
             </div>
-            <button className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </button>
+            <Link href="admin/students" className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </Link>
           </div>
           <div className='flex flex-col items-center justify-end w-full p-4 rounded-3xl bg-green-800 shadow-xl shadow-black gap-y-16'>
             <div className='flex flex-row-reverse items-center justify-center w-full gap-x-24'>
               <span className=''> عدد الاساتذة  </span>
               <span className='font-bold'> 221 </span>
             </div>
-            <button className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </button>
+            <Link href="admin/teachers" className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </Link>
           </div>
           <div className='flex flex-col items-center justify-end w-full p-4 rounded-3xl bg-green-800 shadow-xl shadow-black gap-y-16'>
             <div className='flex flex-row-reverse items-center justify-center w-full gap-x-24'>
               <span className=''> عدد المشرفين  </span>
               <span className='font-bold'> 221 </span>
             </div>
-            <button className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </button>
+            <Link href="admin/admins" className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </Link>
           </div>
           <div className='flex flex-col items-center justify-end w-full p-4 rounded-3xl bg-green-800 shadow-xl shadow-black gap-y-16'>
             <div className='flex flex-row-reverse items-center justify-center w-full gap-x-24'>
               <span className=''> عدد الحلقات  </span>
               <span className='font-bold'> 221 </span>
             </div>
-            <button className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </button>
+            <Link href="admin/courses" className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </Link>
           </div>
           <div className='flex flex-col items-center justify-end w-full p-4 rounded-3xl bg-green-800 shadow-xl shadow-black gap-y-16'>
             <div className='flex flex-row-reverse items-center justify-center w-full gap-x-24'>
               <span className=''> عدد الدروس  </span>
               <span className='font-bold'> 322 </span>
             </div>
-            <button className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </button>
+            <Link href="admin/lessons" className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </Link>
           </div>
           <div className='flex flex-col items-center justify-end w-full p-4 rounded-3xl bg-green-800 shadow-xl shadow-black gap-y-16'>
             <div className='flex flex-row-reverse items-center justify-center w-full gap-x-24'>
               <span className=''> عدد الاعلانات  </span>
               <span className='font-bold'> 192 </span>
             </div>
-            <button className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </button>
+            <Link href="admin/issues" className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </Link>
           </div>
           <div className='flex flex-col items-center justify-end w-full p-4 rounded-3xl bg-green-800 shadow-xl shadow-black gap-y-16'>
             <div className='flex flex-row-reverse items-center justify-center w-full px-8'>
               <span className='text-end'> عدد طلبات الاساتذة للانضمام  </span>
               <span className='font-bold'> 34 </span>
             </div>
-            <button className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </button>
+            <Link href="admin/requests" className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </Link>
 
           </div>
           <div className='flex flex-col items-center justify-end w-full p-4 rounded-3xl bg-green-800 shadow-xl shadow-black gap-y-16'>
             <div className='flex flex-row-reverse items-center justify-center w-full gap-x-24'>
               <span className='text-center'> ايقاف حساب مؤقتا </span>
             </div>
-            <button className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </button>
+            <Link href="admin/students" className='mx-auto font-extralight p-4 bg-gradient-to-r from-green-500 to-green-700 rounded-3xl w-5/6'> عرض </Link>
 
           </div>
         </div>
-        <div className='flex flex-col w-3/5 h-[1000px] p-4 gap-6'>
+        <div className='flex flex-col w-full md:w-3/5 h-[1000px] py-4 md:px-4 gap-y-6 md:gapx-6'>
           <div className='flex w-full justify-center items-center px-2 text-green-800 text-end'>
             <select className='text-end px-4 py-2 rounded-3xl mr-auto bg-green-50'>
               <option>استاذ</option>
